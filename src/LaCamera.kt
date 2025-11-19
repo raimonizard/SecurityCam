97c1fffdecbcf58018f351f968fd5f783fe927d6
raimonizard <raimon.izard@gmail.com>
2025-11-19 15:16:24 +0100

    chore: refactor variables

--- src/LaCamera.kt
+++ src/LaCamera.kt
@@ -5,48 +5,48 @@
  * @since 1.0
  */
 fun main(){
-    val FILES: Int = 10
+    val ROWS: Int = 10
     val COLS: Int = 9
 
-    val fotograma1: Array<IntArray> = Array(FILES){ IntArray(COLS) }
-    fotograma1[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma1[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    fotograma1[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma1[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
-    fotograma1[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
-    fotograma1[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
-    fotograma1[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma1[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    fotograma1[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
-    fotograma1[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
+    val picture1: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
+    picture1[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture1[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    picture1[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture1[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
+    picture1[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
+    picture1[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
+    picture1[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture1[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    picture1[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
+    picture1[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
 
-    val fotograma2: Array<IntArray> = Array(FILES){ IntArray(COLS) }
-    fotograma2[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma2[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    fotograma2[2] = intArrayOf(0,0,0,0,1,0,0,0,0) // braç esquerre amunt
-    fotograma2[3] = intArrayOf(0,1,1,1,1,1,0,0,0) // braç esquerre amunt
-    fotograma2[4] = intArrayOf(0,0,0,0,1,0,1,0,0)
-    fotograma2[5] = intArrayOf(0,0,0,0,1,0,0,1,0)
-    fotograma2[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma2[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    fotograma2[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
-    fotograma2[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
+    val picture2: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
+    picture2[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture2[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    picture2[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture2[3] = intArrayOf(0,1,1,1,1,1,0,0,0)
+    picture2[4] = intArrayOf(0,0,0,0,1,0,1,0,0)
+    picture2[5] = intArrayOf(0,0,0,0,1,0,0,1,0)
+    picture2[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture2[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    picture2[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
+    picture2[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
 
-    val fotograma3: Array<IntArray> = Array(FILES){ IntArray(COLS) }
-    fotograma3[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma3[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    fotograma3[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    fotograma3[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
-    fotograma3[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
-    fotograma3[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
-    fotograma3[6] = intArrayOf(0,0,0,0,1,0,0,0,0) // cama dreta amunt
-    fotograma3[7] = intArrayOf(0,0,0,1,0,1,1,1,0) // cama dreta amunt
-    fotograma3[8] = intArrayOf(0,0,1,0,0,0,0,0,0)
-    fotograma3[9] = intArrayOf(0,1,0,0,0,0,0,0,0)
+    val picture3: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
+    picture3[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture3[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    picture3[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture3[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
+    picture3[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
+    picture3[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
+    picture3[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    picture3[7] = intArrayOf(0,0,0,1,0,1,1,1,0)
+    picture3[8] = intArrayOf(0,0,1,0,0,0,0,0,0)
+    picture3[9] = intArrayOf(0,1,0,0,0,0,0,0,0)
 
-    imprimirFotograma(fotograma1)
-    imprimirFotograma(fotograma2)
-    imprimirFotograma(fotograma3)
+    printPicture(picture1)
+    printPicture(picture2)
+    printPicture(picture3)
 }
 
 /**
@@ -56,7 +56,7 @@ fun main(){
  * @param fotograma Paràmetre d'entrada de tipus matriu d'arrays Array<IntArray>
  * @since 1.0
  */
-fun imprimirFotograma(fotograma: Array<IntArray>){
+fun printPicture(fotograma: Array<IntArray>){
     val RESET: String = "\u001b[0m"
     val WHITE_BOLD: String = "\u001b[1;37m"
     val BLACK_BACKGROUND: String = "\u001b[40m"
@@ -90,7 +90,7 @@ fun imprimirFotograma(fotograma: Array<IntArray>){
  * @author el teu nom
  * @since 2.0
  */
-fun comprovarMoviment(f1: Array<IntArray>, f2: Array<IntArray>): Boolean{
+fun checkMovement(f1: Array<IntArray>, f2: Array<IntArray>): Boolean{
     var movimentDetectat: Boolean = false
 
     //TODO: Completar la funció
