63f612416d47919ea342e6f96651f32d5f631650
raimonizard <raimon.izard@gmail.com>
2025-11-19 15:32:22 +0100

    chore: refactor

--- src/LaCamera.kt
+++ src/LaCamera.kt
@@ -8,47 +8,47 @@ fun main(){
     val ROWS: Int = 10
     val COLS: Int = 9
 
-    val picture1: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
-    picture1[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture1[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    picture1[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture1[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
-    picture1[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
-    picture1[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
-    picture1[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture1[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    picture1[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
-    picture1[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
+    val frame1: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
+    frame1[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame1[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    frame1[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame1[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
+    frame1[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
+    frame1[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
+    frame1[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame1[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    frame1[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
+    frame1[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
 
-    val picture2: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
-    picture2[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture2[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    picture2[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture2[3] = intArrayOf(0,1,1,1,1,1,0,0,0)
-    picture2[4] = intArrayOf(0,0,0,0,1,0,1,0,0)
-    picture2[5] = intArrayOf(0,0,0,0,1,0,0,1,0)
-    picture2[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture2[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    picture2[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
-    picture2[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
+    val frame2: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
+    frame2[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame2[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    frame2[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame2[3] = intArrayOf(0,1,1,1,1,1,0,0,0)
+    frame2[4] = intArrayOf(0,0,0,0,1,0,1,0,0)
+    frame2[5] = intArrayOf(0,0,0,0,1,0,0,1,0)
+    frame2[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame2[7] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    frame2[8] = intArrayOf(0,0,1,0,0,0,1,0,0)
+    frame2[9] = intArrayOf(0,1,0,0,0,0,0,1,0)
 
-    val picture3: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
-    picture3[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture3[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
-    picture3[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture3[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
-    picture3[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
-    picture3[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
-    picture3[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
-    picture3[7] = intArrayOf(0,0,0,1,0,1,1,1,0)
-    picture3[8] = intArrayOf(0,0,1,0,0,0,0,0,0)
-    picture3[9] = intArrayOf(0,1,0,0,0,0,0,0,0)
+    val frame3: Array<IntArray> = Array(ROWS){ IntArray(COLS) }
+    frame3[0] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame3[1] = intArrayOf(0,0,0,1,0,1,0,0,0)
+    frame3[2] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame3[3] = intArrayOf(0,0,0,1,1,1,0,0,0)
+    frame3[4] = intArrayOf(0,0,1,0,1,0,1,0,0)
+    frame3[5] = intArrayOf(0,1,0,0,1,0,0,1,0)
+    frame3[6] = intArrayOf(0,0,0,0,1,0,0,0,0)
+    frame3[7] = intArrayOf(0,0,0,1,0,1,1,1,0)
+    frame3[8] = intArrayOf(0,0,1,0,0,0,0,0,0)
+    frame3[9] = intArrayOf(0,1,0,0,0,0,0,0,0)
 
-    printPicture(picture1)
+    printPicture(frame1)
     println()
-    printPicture(picture2)
+    printPicture(frame2)
     println()
-    printPicture(picture3)
+    printPicture(frame3)
     println()
 }
 
@@ -56,28 +56,28 @@ fun main(){
  * Funció que serveix per imprimir i visualitzar els fotogrames en blanc i negre
  *
  * @author raimonizard
- * @param fotograma Paràmetre d'entrada de tipus matriu d'arrays Array<IntArray>
+ * @param frame Paràmetre d'entrada de tipus matriu d'arrays Array<IntArray>
  * @since 1.0
  */
-fun printPicture(fotograma: Array<IntArray>){
+fun printPicture(frame: Array<IntArray>){
     val RESET: String = "\u001b[0m"
     val WHITE_BOLD: String = "\u001b[1;37m"
     val BLACK_BACKGROUND: String = "\u001b[40m"
 
     print("   ")
-    for (i in 0 until fotograma[0].size)
+    for (i in 0 until frame[0].size)
         print("[$i]")
 
     println("")
 
-    for (f in 0 until fotograma.size) {
+    for (f in 0 until frame.size) {
         print("[$f]")
-        for (c in 0 until fotograma[f].size) {
-            if (fotograma[f][c] == 1) {
-                print("" + BLACK_BACKGROUND + WHITE_BOLD + String.format("%2s ", fotograma[f][c]) + RESET)
+        for (c in 0 until frame[f].size) {
+            if (frame[f][c] == 1) {
+                print("" + BLACK_BACKGROUND + WHITE_BOLD + String.format("%2s ", frame[f][c]) + RESET)
             }
             else {
-                print(String.format("%2s ",fotograma[f][c]))
+                print(String.format("%2s ",frame[f][c]))
             }
 
         }
